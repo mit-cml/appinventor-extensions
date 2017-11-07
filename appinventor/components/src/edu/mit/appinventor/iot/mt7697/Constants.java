96ad78d90eac2a7209e659d107118bbb1b8fe879
jerry73204 <jerry73204@gmail.com>
2017-11-07 18:00:10 +0800

    Add new constants for MT7697 GPIO interface

--- appinventor/components/src/edu/mit/appinventor/iot/mt7697/Constants.java
+++ appinventor/components/src/edu/mit/appinventor/iot/mt7697/Constants.java
@@ -4,6 +4,66 @@
 package edu.mit.appinventor.iot.mt7697;
 
 class Constants {
+  public static final String GPIO00_SERVICE = "b8b1b700-6422-4d6d-bf79-b76e47e9ac86";
+  public static final String GPIO00_MODE_CHARACTERISTIC = "b8b1b700-6422-4d6d-bf79-b76e47e9ac86";
+  public static final String GPIO00_DATA_CHARACTERISTIC = "b8b1b701-6422-4d6d-bf79-b76e47e9ac86";
+
+  public static final String GPIO02_SERVICE = "682e2900-cd85-4578-b0ac-37c89b1c8c3a";
+  public static final String GPIO02_MODE_CHARACTERISTIC = "682e2900-cd85-4578-b0ac-37c89b1c8c3a";
+  public static final String GPIO02_DATA_CHARACTERISTIC = "682e2901-cd85-4578-b0ac-37c89b1c8c3a";
+
+  public static final String GPIO03_SERVICE = "56b16f00-2193-4cde-8e09-c3ee4c72dc78";
+  public static final String GPIO03_MODE_CHARACTERISTIC = "56b16f00-2193-4cde-8e09-c3ee4c72dc78";
+  public static final String GPIO03_DATA_CHARACTERISTIC = "56b16f01-2193-4cde-8e09-c3ee4c72dc78";
+
+  public static final String GPIO29_SERVICE = "b053a500-8e23-4bfd-8f88-4af09fd9040b";
+  public static final String GPIO29_MODE_CHARACTERISTIC = "b053a500-8e23-4bfd-8f88-4af09fd9040b";
+  public static final String GPIO29_DATA_CHARACTERISTIC = "b053a501-8e23-4bfd-8f88-4af09fd9040b";
+
+  public static final String GPIO30_SERVICE = "9aa1fa00-ebc3-4fb6-ba4a-1bb79b8a9d78";
+  public static final String GPIO30_MODE_CHARACTERISTIC = "9aa1fa00-ebc3-4fb6-ba4a-1bb79b8a9d78";
+  public static final String GPIO30_DATA_CHARACTERISTIC = "9aa1fa01-ebc3-4fb6-ba4a-1bb79b8a9d78";
+
+  public static final String GPIO31_SERVICE = "981a6300-c513-40dc-ad0c-c873e06ff18b";
+  public static final String GPIO31_MODE_CHARACTERISTIC = "981a6300-c513-40dc-ad0c-c873e06ff18b";
+  public static final String GPIO31_DATA_CHARACTERISTIC = "981a6301-c513-40dc-ad0c-c873e06ff18b";
+
+  public static final String GPIO32_SERVICE = "9126b700-1a1e-48df-82f7-4100a9fb687b";
+  public static final String GPIO32_MODE_CHARACTERISTIC = "9126b700-1a1e-48df-82f7-4100a9fb687b";
+  public static final String GPIO32_DATA_CHARACTERISTIC = "9126b701-1a1e-48df-82f7-4100a9fb687b";
+
+  public static final String GPIO33_SERVICE = "7ed8d200-6a9f-4d8b-b50f-e8f4b78667b2";
+  public static final String GPIO33_MODE_CHARACTERISTIC = "7ed8d200-6a9f-4d8b-b50f-e8f4b78667b2";
+  public static final String GPIO33_DATA_CHARACTERISTIC = "7ed8d201-6a9f-4d8b-b50f-e8f4b78667b2";
+
+  public static final String GPIO34_SERVICE = "525d9c00-3995-480c-80d6-98cf3a8420e9";
+  public static final String GPIO34_MODE_CHARACTERISTIC = "525d9c00-3995-480c-80d6-98cf3a8420e9";
+  public static final String GPIO34_DATA_CHARACTERISTIC = "525d9c01-3995-480c-80d6-98cf3a8420e9";
+
+  public static final String GPIO38_SERVICE = "756bd900-81b9-4565-9ce2-f6f2dfabbbec";
+  public static final String GPIO38_MODE_CHARACTERISTIC = "756bd900-81b9-4565-9ce2-f6f2dfabbbec";
+  public static final String GPIO38_DATA_CHARACTERISTIC = "756bd901-81b9-4565-9ce2-f6f2dfabbbec";
+
+  public static final String GPIO39_SERVICE = "b6d08900-99e4-4c53-89ef-4039b61ffb19";
+  public static final String GPIO39_MODE_CHARACTERISTIC = "b6d08900-99e4-4c53-89ef-4039b61ffb19";
+  public static final String GPIO39_DATA_CHARACTERISTIC = "b6d08901-99e4-4c53-89ef-4039b61ffb19";
+
+  public static final String GPIO57_SERVICE = "cd893c00-de11-4cb7-9436-b9f129f5c249";
+  public static final String GPIO57_MODE_CHARACTERISTIC = "cd893c00-de11-4cb7-9436-b9f129f5c249";
+  public static final String GPIO57_DATA_CHARACTERISTIC = "cd893c01-de11-4cb7-9436-b9f129f5c249";
+
+  public static final String GPIO58_SERVICE = "42b07000-ce29-4feb-9fc1-5ed55cf9f3af";
+  public static final String GPIO58_MODE_CHARACTERISTIC = "42b07000-ce29-4feb-9fc1-5ed55cf9f3af";
+  public static final String GPIO58_DATA_CHARACTERISTIC = "42b07001-ce29-4feb-9fc1-5ed55cf9f3af";
+
+  public static final String GPIO59_SERVICE = "4433a600-40eb-462a-8a98-22e326f87eca";
+  public static final String GPIO59_MODE_CHARACTERISTIC = "4433a600-40eb-462a-8a98-22e326f87eca";
+  public static final String GPIO59_DATA_CHARACTERISTIC = "4433a601-40eb-462a-8a98-22e326f87eca";
+
+  public static final String GPIO60_SERVICE = "6dce9900-d064-4c5d-ae39-9380f52712d6";
+  public static final String GPIO60_MODE_CHARACTERISTIC = "6dce9900-d064-4c5d-ae39-9380f52712d6";
+  public static final String GPIO60_DATA_CHARACTERISTIC = "6dce9901-d064-4c5d-ae39-9380f52712d6";
+
   public static final String PIN_SERVICE = "a56ada00-ed09-11e5-9c97-0002a5d5c51b";
   public static final String ANALOG_PIN_CHARACTERISTIC = "a56ada02-ed09-11e5-9c97-0002a5d5c51b";
   public static final String DIGITAL_PIN_CHARACTERISTIC = "a56ada03-ed09-11e5-9c97-0002a5d5c51b";
