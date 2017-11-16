44d0145edffc5d8e593a648f9234e6234fcedb1b
YuanYuYuan <az6980522@gmail.com>
2017-11-16 21:31:51 +0800

    fixed the reallocated bug of vector of multi-characteristic

--- arduino-sources/MT7697/MT7697.ino
+++ arduino-sources/MT7697/MT7697.ino
@@ -12,74 +12,74 @@ void setup()
 
 void loop()
 {
-    for (int idx = 0; idx < GPIO_UUID_PROFILES_SIZE; idx += 1)
-    {
-        auto& lble_ref = GPIO_LBLE_PROFILES[idx];
-        const int pin = lble_ref.pin;
-
-        // set pin mode if requested
-        if (lble_ref.mode_char->isWritten())
-        {
-            const int value = lble_ref.mode_char->getValue();
-            lble_ref.mode = value;
-
-            switch (value)
-            {
-            case PIN_MODE_NONE:
-                break;
-            case PIN_MODE_ANALOG_READ:
-            case PIN_MODE_DIGITAL_READ:
-            {
-                pinMode(pin, INPUT);
-                break;
-            }
-            case PIN_MODE_ANALOG_WRITE:
-            case PIN_MODE_DIGITAL_WRITE:
-            {
-                pinMode(pin, OUTPUT);
-                break;
-            }
-            case PIN_MODE_SERVO:
-            {
-                // TODO
-                break;
-            }
-            default:
-                assert(0);
-            }
-        }
-
-        // execute I/O according to its mode
-        const int mode = lble_ref.mode;
-        switch (mode)
-        {
-        case PIN_MODE_ANALOG_READ:
-        {
-            // TODO
-            break;
-        }
-        case PIN_MODE_ANALOG_WRITE:
-        {
-            // TODO
-            break;
-        }
-        case PIN_MODE_DIGITAL_READ:
-        {
-            // TODO
-            break;
-        }
-        case PIN_MODE_DIGITAL_WRITE:
-        {
-            // TODO
-            break;
-        }
-        case PIN_MODE_SERVO:
-        {
-            // TODO
-            break;
-        }
-        default:
-            assert(mode == PIN_MODE_NONE);
-        }
-    }
+    // for (int idx = 0; idx < GPIO_UUID_PROFILES_SIZE; idx += 1)
+    // {
+    //     auto& lble_ref = GPIO_LBLE_PROFILES[idx];
+    //     const int pin = lble_ref.uuid_profile->pin;
+    //
+    //     // set pin mode if requested
+    //     if (lble_ref.mode_char.isWritten())
+    //     {
+    //         const int value = lble_ref.mode_char.getValue();
+    //         lble_ref.mode = value;
+    //
+    //         switch (value)
+    //         {
+    //         case PIN_MODE_NONE:
+    //             break;
+    //         case PIN_MODE_ANALOG_READ:
+    //         case PIN_MODE_DIGITAL_READ:
+    //         {
+    //             pinMode(pin, INPUT);
+    //             break;
+    //         }
+    //         case PIN_MODE_ANALOG_WRITE:
+    //         case PIN_MODE_DIGITAL_WRITE:
+    //         {
+    //             pinMode(pin, OUTPUT);
+    //             break;
+    //         }
+    //         case PIN_MODE_SERVO:
+    //         {
+    //             // TODO
+    //             break;
+    //         }
+    //         default:
+    //             assert(0);
+    //         }
+    //     }
+    //
+    //     // execute I/O according to its mode
+    //     const int mode = lble_ref.mode;
+    //     switch (mode)
+    //     {
+    //     case PIN_MODE_ANALOG_READ:
+    //     {
+    //         // TODO
+    //         break;
+    //     }
+    //     case PIN_MODE_ANALOG_WRITE:
+    //     {
+    //         // TODO
+    //         break;
+    //     }
+    //     case PIN_MODE_DIGITAL_READ:
+    //     {
+    //         // TODO
+    //         break;
+    //     }
+    //     case PIN_MODE_DIGITAL_WRITE:
+    //     {
+    //         // TODO
+    //         break;
+    //     }
+    //     case PIN_MODE_SERVO:
+    //     {
+    //         // TODO
+    //         break;
+    //     }
+    //     default:
+    //         assert(mode == PIN_MODE_NONE);
+    //     }
+    // }
 }
