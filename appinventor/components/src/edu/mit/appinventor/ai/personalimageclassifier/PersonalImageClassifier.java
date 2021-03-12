e0e7454c213f9ea2e1de342242e737b9155ed760
Evan W. Patton <ewpatton@mit.edu>
2021-03-12 08:48:01 -0500

    Implement activity lifecycle support
    Change-Id: I0c37194551bbd3fa923825659a0f8ace16387252

--- appinventor/components/src/edu/mit/appinventor/ai/personalimageclassifier/PersonalImageClassifier.java
+++ appinventor/components/src/edu/mit/appinventor/ai/personalimageclassifier/PersonalImageClassifier.java
@@ -33,6 +33,9 @@ import com.google.appinventor.components.runtime.AndroidNonvisibleComponent;
 import com.google.appinventor.components.runtime.Component;
 import com.google.appinventor.components.runtime.EventDispatcher;
 import com.google.appinventor.components.runtime.Form;
+import com.google.appinventor.components.runtime.OnClearListener;
+import com.google.appinventor.components.runtime.OnPauseListener;
+import com.google.appinventor.components.runtime.OnResumeListener;
 import com.google.appinventor.components.runtime.WebViewer;
 import com.google.appinventor.components.runtime.util.ErrorMessages;
 import com.google.appinventor.components.runtime.util.JsonUtil;
@@ -71,7 +74,9 @@ import java.util.zip.ZipInputStream;
 @SimpleObject(external = true)
 @UsesAssets(fileNames = "personal_image_classifier.html, personal_image_classifier.js, mobilenet_group1-shard1of1, mobilenet_group10-shard1of1, mobilenet_group11-shard1of1, mobilenet_group12-shard1of1, mobilenet_group13-shard1of1, mobilenet_group14-shard1of1, mobilenet_group15-shard1of1, mobilenet_group16-shard1of1, mobilenet_group17-shard1of1, mobilenet_group18-shard1of1, mobilenet_group19-shard1of1, mobilenet_group2-shard1of1, mobilenet_group20-shard1of1, mobilenet_group21-shard1of1, mobilenet_group22-shard1of1, mobilenet_group23-shard1of1, mobilenet_group24-shard1of1, mobilenet_group25-shard1of1, mobilenet_group26-shard1of1, mobilenet_group27-shard1of1, mobilenet_group28-shard1of1, mobilenet_group29-shard1of1, mobilenet_group3-shard1of1, mobilenet_group30-shard1of1, mobilenet_group31-shard1of1, mobilenet_group32-shard1of1, mobilenet_group33-shard1of1, mobilenet_group34-shard1of1, mobilenet_group35-shard1of1, mobilenet_group36-shard1of1, mobilenet_group37-shard1of1, mobilenet_group38-shard1of1, mobilenet_group39-shard1of1, mobilenet_group4-shard1of1, mobilenet_group40-shard1of1, mobilenet_group41-shard1of1, mobilenet_group42-shard1of1, mobilenet_group43-shard1of1, mobilenet_group44-shard1of1, mobilenet_group45-shard1of1, mobilenet_group46-shard1of1, mobilenet_group47-shard1of1, mobilenet_group48-shard1of1, mobilenet_group49-shard1of1, mobilenet_group5-shard1of1, mobilenet_group50-shard1of1, mobilenet_group51-shard1of1, mobilenet_group52-shard1of1, mobilenet_group53-shard1of1, mobilenet_group54-shard1of1, mobilenet_group55-shard1of1, mobilenet_group6-shard1of1, mobilenet_group7-shard1of1, mobilenet_group8-shard1of1, mobilenet_group9-shard1of1, mobilenet_model.json, squeezenet_group1-shard1of1, squeezenet_model.json, tfjs-0.13.2.js")
 @UsesPermissions(permissionNames = "android.permission.INTERNET, android.permission.CAMERA")
-public final class PersonalImageClassifier extends AndroidNonvisibleComponent implements Component {
+public final class PersonalImageClassifier extends AndroidNonvisibleComponent
+    implements Component, OnPauseListener, OnResumeListener, OnClearListener {
+
   private static final String LOG_TAG = PersonalImageClassifier.class.getSimpleName();
   private static final int IMAGE_WIDTH = 500;
   private static final int IMAGE_QUALITY = 100;
@@ -337,6 +342,29 @@ public final class PersonalImageClassifier extends AndroidNonvisibleComponent im
   public void Error(final int errorCode) {
     EventDispatcher.dispatchEvent(this, "Error", errorCode);
   }
+
+  ///REGION: Lifecycle handling
+
+  @Override
+  public void onPause() {
+    if (MODE_VIDEO.equals(inputMode)) {
+      webview.evaluateJavascript("stopVideo();", null);
+    }
+  }
+
+  @Override
+  public void onResume() {
+    if (MODE_VIDEO.equals(inputMode)) {
+      webview.evaluateJavascript("startVideo();", null);
+    }
+  }
+
+  @Override
+  public void onClear() {
+    webview.evaluateJavascript("stopVideo();", null);
+  }
+
+  ///ENDREGION
 
   Form getForm() {
     return form;
